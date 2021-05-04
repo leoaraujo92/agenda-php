a932bb57034aad074808bde74f88f96d34341324
leoaraujo92 <leonardo.araujo2008@gmail.com>
2021-05-04 19:35:51 -0300

    Definindo templates

---
+++ templates/header.php
@@ -0,0 +1,35 @@
+<?php
+
+    include_once("config/url.php");
+    include_once("config/connection.php");
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda de contatos</title>
+    <!--BOOTSTRAP-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css" integrity="sha512-P5MgMn1jBN01asBgU0z60Qk4QxiXo86+wlFahKrsQf37c9cro517WzVSPPV1tDKzhku2iJ2FVgL67wG03SGnNA==" crossorigin="anonymous" />
+    <!--FONTAWESOME-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
+    <!--CSS-->
+    <link rel="stylesheet" href="<?= $BASE_URL ?>css/styles.css">
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <a class="nav-brand" href="<?= $BASE_URL ?>index.php">
+                <img src="<?= $BASE_URL ?>img/logo.svg" alt="Agenda">
+            </a>
+            <div>
+                <div class="navbar-nav">
+                    <a href="<?= $BASE_URL ?>index.php" id ="home-link"class="nav-link active">Agenda</a>
+                    <a href="<?= $BASE_URL ?>create.php" id ="home-link"class="nav-link active">Adicionar Contato</a>
+                </div>
+            </div>
+        </nav>
+    </header>
